e176f921ad7239073ed7fbe64e7762b7f0c5d485
Sales <contabilgs3@gmail.com>
2025-08-08 10:07:01 -0300

    :tada: Commit Inicial

---
+++ server.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+
+// Autenticação do Token
+const auth = function(req, res, next){
+    
+    if(!req.headers.token || req.headers.token != process.env.TOKEN){
+        res.status(401).send({
+            atuh: "Não autorizado!"
+        })
+    }
+}
+
+// Servidor Express
+const express = require('express')
+const cors = require('cors')
+const http = require('http')
+
+const app = express()
+app.use(express.json(), cors({ origin: "*" }), auth)
+
+const server = http.createServer(app)
+
+server.listen(process.env.PORT, (err) => {
+    if(err) throw err
+    console.log('Servidor Live! Porta: ', process.env.PORT)
+})
+
+// Conexão com Banco de Dados
+const mysql = require('mysql2')
+
+const pool = mysql.createPool(process.env.DBURL)
+
+pool.getConnection((err, con) => {
+    if(err) throw err
+    console.log("Banco de Dados conectado!")
+    con.release()
+})
+
+// Exporta os módulos
+
+module.exports = {
+    app: app,
+    pool: pool
+}
